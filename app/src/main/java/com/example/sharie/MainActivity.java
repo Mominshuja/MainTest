cf822c2f04bcf5c4718d9a0af41801d497825a63
MOMIN <mominshuja>
2021-03-08 21:08:38 +0530

    Shuja MOmin 8/3 21

--- app/src/main/java/com/example/sharie/MainActivity.java
+++ app/src/main/java/com/example/sharie/MainActivity.java
@@ -12,3 +12,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
